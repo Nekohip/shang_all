70995a7ebf81f4202a7d9fad61431cf46b1eb85d
Shang <konekohip@tuta.io>
2025-10-08 10:23:33 +0800

    delete

--- c#/practice1/obj/Debug/net9.0/practice1.AssemblyInfo.cs
+++ c#/practice1/obj/Debug/net9.0/practice1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practice1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f5f5a22174e82e7ae550fe7419828c9e09ea134")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423d2ba1387248a364045cc3e185b8ce9776e7c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("practice1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practice1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
