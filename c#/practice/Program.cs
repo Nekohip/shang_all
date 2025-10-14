7107617f07d88c490ecfd20cea6ad3621e56d4ba
Shang <konekohip@tuta.io>
2025-10-14 16:36:20 +0800

    10_14_class

---
+++ c#/practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CircleArea
+{
+    class Circle
+    {
+        private double r = 0;  //私有變數      
+        public void input()  //方法
+        {
+            Console.Write("請輸入圓半徑:");
+            r = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void 
+    }
+    //Main
+    class CircleArea
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
