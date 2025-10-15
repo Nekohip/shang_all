c6cac2ffa665f60069032e8f12f64f6398f79226
Shang <konekohip@tuta.io>
2025-10-15 16:36:56 +0800

    10_15_class

--- c#/practice/Program.cs
+++ c#/practice/Program.cs
@@ -5,8 +5,11 @@ namespace CircleArea
 {
     class Circle
     {
-        private double r = 0;  //私有變數      
-        public void input()  //方法
+        //私有變數，無法在其他class叫出，只能在此class使用
+        private double r = 0;
+        //方法，void是回傳型別，可以寫return回傳
+        //括弧裡是接收參數，可接受數個如:(int a, int b))  
+        public void input()
         {
             Console.Write("請輸入圓半徑:");
             r = Convert.ToDouble(Console.ReadLine());
@@ -23,13 +26,13 @@ namespace CircleArea
         }
     }
     //Main
-    class CircleArea
+    class ExecuteCircleArea
     {
         static void Main(string[] args)
         {
-            Circle c = new Circle();
-            c.input();
-            c.display();
+            Circle c = new Circle();  //創建物件
+            c.input();  //呼叫方法
+            c.display();  
         }
     }
 }
