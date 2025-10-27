0209e1427caabaa46a03c9288335e8424fa69e4d
konekohip <konekohip@tuta.io>
2025-10-28 00:32:41 +0800

    理解菱形-對角公式，新增圖解

--- php_basic/teacher3.php
+++ php_basic/teacher3.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        *{
+            /*使用每個字都一樣大的字型*/
+            font-family:'Courier New', 'Courier', 'monospace';
+        }
+    </style>
 </head>
 <body>
     <?php
@@ -28,6 +34,40 @@
 
       echo "<br>";
     }
+
+echo "菱形-對角線<br>";
+$x=9;
+$y=0;
+
+if($x%2==0){
+    $x=$x+1;
+}
+$mid=floor($x/2);
+
+for($i=0;$i<$x;$i++){
+    if($i>$mid){
+        $y=$y-1;
+    }else{
+        $y=$i;
+    }
+
+    //echo $i."-".$y;
+
+
+    for($k=0;$k<$mid-$y;$k++){
+        echo "&nbsp;";
+    }
+    //echo $y;
+    for($j=0;$j<2*$y+1;$j++){
+        
+        if($j==0 || $j==2*$y || $j==$y || $i==$mid){
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }
+    }
+    echo "<br>";
+}
     ?>
 </body>
 </html>
