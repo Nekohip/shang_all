f0874f24461e91a4b07c93426744af6e9ef7a3e3
shang <konekohip@tuta.io>
2025-11-11 16:33:37 +0800

    新增檔案，完成index

---
+++ php_web/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>網頁傳值練習</title>
+</head>
+<body>
+    <h2>BMI計算</h2>
+    <ul>
+        <li>建立一個可以輸入身高和體重的表單畫面</li>
+        <li>按下"計算BMI"按鈕後，在另一個頁面顯示BMI值</li>
+        <a href="./bmi/bmi.php">前往</a>
+    </ul>
+
+    <h2>登入檢查</h2>
+    <ul>
+        <li>建立一個可以輸入帳號和密碼的表單畫面</li>
+        <li>輸入帳號密碼，按下"登入"按鈕後，在另一個頁面顯示帳號密碼是否正確。</li>
+        <a href="./login/login.php">前往</a>
+    </ul>
+
+    <h2>設計一個網頁版萬年曆，只需顯示西元年月日</h2>
+    <ul>
+        <li>以月為單位來顯示一個月中的日期</li>
+        <li>有上一個月，下一個月的連結來切換月份</li>
+        <li>可以不同的顏色或樣式來強調當日或周末</li>
+        <a href="./calender/calender.php">前往</a>
+    </ul>
+</body>
+</html>
