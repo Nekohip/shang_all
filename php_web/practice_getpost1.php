5d644826dad8bebcba25e42ca3b2e34452c62345
shang <konekohip@tuta.io>
2025-11-12 15:28:38 +0800

    新增練習檔案

---
+++ php_web/practice_getpost1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action='practice_getpost2.php' method='get'>
+        <label>名字</label><input type='text' name='fname'><br>
+        <label>年齡</label><input type='text' name='age'>
+        <input type='submit' value='提交'>
+    </form>
+</body>
+</html>
---
+++ php_web/practice_getpost2.php
@@ -0,0 +1,6 @@
+<?php
+$name =  $_GET['fname'];
+$age = $_GET['age'];
+echo '歡迎' . $name . '!<br>';
+echo '你的年齡是:' . $age . '歲';
+?>
